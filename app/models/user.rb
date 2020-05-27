ecce44fa9912d46283637e27214922f7ec237014
Kensuke_Mitsui <mitsui.investment@gmail.com>
2020-05-28 01:27:21 +0900

    deviseパスワード修正（バリデーション）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,23 @@ class User < ApplicationRecord
 
 #バリデーション
   validates :nickname,           presence: true
-  validates :encrypted_password, presence: true, length: {minimum: 7 }, confirmation: true
+  validates :password,           presence: true, length: {minimum: 7 }, confirmation: true
   validates :family_name,        presence: true
   validates :first_name,         presence: true
   validates :family_name_kana,   presence: true
   validates :first_name_kana,    presence: true
   validates :birthday,           presence: true
   
+  # def password_required?
+  #   @user = User.encrypted_password
+  #   if @user < 7
+  #     render :new
+  #   else
+  #     super
+  #   end      
+  # end
+  # スーパーライドを上書きするか？
+
   #deviseのバリデーション
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
